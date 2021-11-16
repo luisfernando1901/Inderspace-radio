be592ecae621c42571ab8c911a19db89db005ce7
Luis Fernando Nestares Valverde <68039881+luisfernando1901@users.noreply.github.com>
2021-11-15 22:47:10 -0500

    Se cambió el url a producción.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000',
-  //baseUrl: 'https://inderspace-radio.uc.r.appspot.com'
+  //baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://inderspace-radio.uc.r.appspot.com'
 };
